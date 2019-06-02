92d9c6896372cb2ce0a346c64564e75806c7254b
Pedro González <pgonzalezalo@uoc.edu>
2019-06-03 00:22:06 +0200

    Update the is_silence function in lib

--- lib/util.py
+++ lib/util.py
@@ -1,6 +1,7 @@
+import numpy as np
 # Determines if a piece of audio is a silence by counting the number of
 # points under a given amplitude thresold 
-def is_silence(audio_chunk,max_amplitude=0.001,thresold_samples=0.90):
+def is_silence(audio_chunk,max_amplitude=0.001,thresold_samples=0.70):
     samplePoints = len(audio_chunk)
     silencePoints = sum(np.abs(audio_chunk)<max_amplitude)
     
