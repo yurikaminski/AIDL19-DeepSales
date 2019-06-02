c560b0a552d75f72eefbf9dc5b29b2d483c762fa
Pedro González <pgonzalezalo@uoc.edu>
2019-06-03 00:02:09 +0200

    Created folder lib with a python module with support functions (ex. is_silence function)

---
+++ lib/util.py
@@ -0,0 +1,8 @@
+# Determines if a piece of audio is a silence by counting the number of
+# points under a given amplitude thresold 
+def is_silence(audio_chunk,max_amplitude=0.001,thresold_samples=0.90):
+    samplePoints = len(audio_chunk)
+    silencePoints = sum(np.abs(audio_chunk)<max_amplitude)
+    
+    return(silencePoints / samplePoints > thresold_samples)
+
